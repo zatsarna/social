d952c626029f76fda4add7dee6327ca3632267a6
zatsarna <lyudazats@gmail.com>
2023-08-24 12:05:27 -0400

    Side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,7 +3,8 @@ import photo from '../../assets/images/noPhotoUser.jpg'
 
 import s from './Users.module.css'
 import axios from 'axios';
-export type UserResponceType={
+
+export type UserResponceType = {
     name: string,
     id: number,
     photos: {
@@ -13,7 +14,7 @@ export type UserResponceType={
     status?: null | string,
     followed: boolean
 }
-export type UsersResponceType={
+export type UsersResponceType = {
     items: UserResponceType[],
     totalCount?: number,
     error?: string | null
@@ -24,32 +25,36 @@ export const Users = (props: {
     toggleFollow: (userId: number) => void,
     setUsers: (users: UserResponceType[]) => void
 }) => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get<UsersResponceType>('https://social-network.samuraijs.com/api/1.0/users').then(res => props.setUsers(res.data.items))
 
-    if (props.users.length===0){
-        axios.get<UsersResponceType>('https://social-network.samuraijs.com/api/1.0/users').then(res =>props.setUsers(res.data.items))
-
+        }
     }
 
+
     return (<>
-        <ul>
-            {props.users.map(u => <li key={u.id}>
-                <div><img src={u.photos.small ? u.photos.small : photo} alt={'profile photo'} className={s.imgSize}/>
-                    <button onClick={()=>props.toggleFollow(u.id)}>{u.followed ? 'Unfollow' : 'Follow'}</button>
-                </div>
-                <div>
+            <button onClick={getUsers}>Get Users</button>
+            <ul>
+                {props.users.map(u => <li key={u.id}>
+                    <div><img src={u.photos.small ? u.photos.small : photo} alt={'profile photo'}
+                              className={s.imgSize}/>
+                        <button onClick={() => props.toggleFollow(u.id)}>{u.followed ? 'Unfollow' : 'Follow'}</button>
+                    </div>
                     <div>
-                        <span>{u.name}</span>
-                        <p>{u.status}</p>
+                        <div>
+                            <span>{u.name}</span>
+                            <p>{u.status}</p>
+                        </div>
+
+                        <div><span>{'u.location.country'}</span> <span>{'u.location.city'}</span></div>
+
                     </div>
 
-                    <div><span>{'u.location.country'}</span> <span>{'u.location.city'}</span></div>
 
-                </div>
-
-
-            </li>)}
-        </ul>
-        <button onClick={()=>props.setUsers(props.users)}>Show more</button>
+                </li>)}
+            </ul>
+            <button onClick={() => props.setUsers(props.users)}>Show more</button>
         </>
     );
 };
